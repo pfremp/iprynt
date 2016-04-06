e340ce97736f0f1a6410a85c9eafc61863237c6e
Patrick Frempong <patrickfrempong@gmail.com>
2016-04-06 16:58:43 +0100

    added mobile menu functionality

---
+++ assets/js/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+                  
+    $(".header__toggle").on("click", function(){
+        
+        $(".header__nav ul").toggleClass("open");
+        $(".banner__tagline--mobile").toggleClass("open");
+
+    });
+           
+});
+                
