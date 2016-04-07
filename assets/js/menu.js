eb67195957322dd404e929e67516d04c45f969d0
Patrick Frempong <patrickfrempong@gmail.com>
2016-04-07 15:16:30 +0100

    updated css and js to hide main banner when mobile menu is toggled

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         
         $(".header__nav ul").toggleClass("open");
         $(".banner__tagline--mobile").toggleClass("open");
+        $(".banner__tagline").toggleClass("open");
 
     });
            
